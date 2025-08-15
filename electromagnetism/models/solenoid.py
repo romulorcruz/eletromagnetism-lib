3c820004d213e0cff38245404e7f48f594009e0c
Romulo Emanuel Rabelo Cruz <rerc877@gmail.com>
2025-08-15 15:19:10 -0300

    Add: initial solenoid generator

---
+++ electromagnetism/models/solenoid.py
@@ -0,0 +1,24 @@
+from numpy import array, ndarray, loadtxt, moveaxis, newaxis, cross, concatenate, shape
+from numpy.linalg import norm
+
+class Solenoid(Coil):
+    def __init__(self, n_turns: int, z_initial_point: float, z_final_point: float, radius: float, max_seg_len: float,
+                 *, crossSectionalArea: float = 1.0, resistivity: float = 1.7e-8,invertRAxis: bool = False):
+
+        if n_turns <= 0:
+            raise ValueError("Invalid parameters.")
+        if radius <= 0:
+            raise ValueError("Invalid parameters.")
+        if max_seg_len <= 0:
+            raise ValueError("Invalid parameters.")
+        self.n_turns = n_turns
+        self.z_initial_point = z_initial_point
+        self.z_final_point = z_final_point
+        self.radius = radius
+        self.max_seg_len = max_seg_len
+        self._resistivity = resistivity
+        self._crossSectionalArea = crossSectionalArea
+        self.coilPath = np.array(self.__helicoid())
+ 
+ 
+        super().__init__(self.coilPath, invertRAxis=invertRAxis, crossSectionalArea=crossSectionalArea, resistivity=resistivity)
